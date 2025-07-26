4098af2196f6d1bd379c8efcbe85d9d01c421a9c
ThePrimeagen <the.primeagen@aol.com>
2025-07-25 22:59:25 -0500

    clock added

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("My Awesome Asteroids Game!")
     game = True
     while(game == True):
         for event in pygame.event.get():
@@ -18,6 +21,7 @@ def main():
         screen.fill(color1)
 
 
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
